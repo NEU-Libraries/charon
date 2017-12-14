9a69203a4c748b855ada02f765b1d4e7cd871e53
David Cliff <dgcliff@northeastern.edu>
2017-12-14 15:53:00 -0500

    Different ver

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ task :remote_environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use', 'ruby-2.3.3@global'
+  invoke :'rvm:use', 'ruby-2.3.3'
 end
 
 # Put any custom commands you need to run at setup
