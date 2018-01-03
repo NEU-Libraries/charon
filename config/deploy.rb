2e1c5408e8c70032c487d2fdcfa73f51ea35ea2f
David Cliff <dgcliff@northeastern.edu>
2018-01-03 16:26:52 -0500

    Removing deprecated and not needed env arg

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ task :setup do
 end
 
 desc "Deploys the current version to the server."
-task :deploy => :environment do
+task :deploy do
   # uncomment this line to make sure you pushed your local branch to the remote origin
   invoke :'git:ensure_pushed'
   deploy do
