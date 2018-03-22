8408dcd9a6c65d7db98444423372595fd2d25de3
David Cliff <dgcliff@northeastern.edu>
2018-03-22 11:35:38 -0400

    Trying to fix invalid byte sequence in US-ASCII

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,6 +51,8 @@ task :deploy do
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
+    invoke :'export LANG=en_US.UTF-8; export LANGUAGE=en_US.UTF-8; export LC_ALL=en_US.UTF-8'
+
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
