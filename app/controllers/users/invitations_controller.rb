4d5cbbf45482ab0a0e3498652e1b37227dcb66be
David Cliff <dgcliff@northeastern.edu>
2018-04-02 12:01:57 -0400

    Change email form value from stock, regex check for basic validity. Still need to create custom error for bad email strings

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -19,8 +19,19 @@ class Users::InvitationsController < Devise::InvitationsController
     end
 
     # Are there email addresses?
+    if params[:emails].blank?
+      render :file => "#{Rails.root}/public/400.html", :status => 400, :layout => false and return
+    end
 
     # Are they valid (basic regex)?
+    emails = params[:emails]
+
+    # regex from https://stackoverflow.com/questions/742451/what-is-the-simplest-regular-expression-to-validate-emails-to-not-accept-them-bl
+    emails.each do |email_str|
+      if !(/^[^@\s]+@[^@\s]+\.[^@\s]+$/.match?(email_str))
+        # Raise custom error and flash to tell the user their input was bad
+      end
+    end
 
     # Loop and process
   end
