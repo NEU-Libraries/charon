24b8fab1888fca5a60e1a136644a6517a39c3fd4
David Cliff <dgcliff@northeastern.edu>
2018-04-02 12:11:00 -0400

    Standard 400 for email strings that make it to the controller. Use JS validation to prevent genuine user error. Redirect to admin set edit page with flash notice of success when successful.

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -29,7 +29,6 @@ class Users::InvitationsController < Devise::InvitationsController
     # regex from https://stackoverflow.com/questions/742451/what-is-the-simplest-regular-expression-to-validate-emails-to-not-accept-them-bl
     emails.each do |email_str|
       if !(/^[^@\s]+@[^@\s]+\.[^@\s]+$/.match?(email_str))
-        # Raise custom error and flash to tell the user their input was bad
         render :file => "#{Rails.root}/public/400.html", :status => 400, :layout => false and return
       end
     end
@@ -38,5 +37,7 @@ class Users::InvitationsController < Devise::InvitationsController
     emails.each do |email|
       User.invite!(:email => email)
     end
+
+    redirect_to edit_admin_admin_set_path(params[:id])
   end
 end
