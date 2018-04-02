734336935c84f4b29947f7bd38fac62f4901ba75
David Cliff <dgcliff@northeastern.edu>
2018-04-02 11:34:21 -0400

    Stubbing out logic for mass invite

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,4 +1,15 @@
 class Users::InvitationsController < Devise::InvitationsController
   def mass_invitation
+    # Are they logged in?
+
+    # Is there a pid, and is it an admin set?
+
+    # Does the user have permissions?
+
+    # Are there email addresses?
+
+    # Are they valid (basic regex)?
+
+    # Loop and process
   end
 end
