cdfa3bddae20a49ed82c37b4d25a8f5f8b6be343
David Cliff <dgcliff@northeastern.edu>
2018-04-02 11:38:13 -0400

    Must login to perform mass invite else 403 forbidden

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,6 +1,7 @@
 class Users::InvitationsController < Devise::InvitationsController
   def mass_invitation
     # Are they logged in?
+    if !current_user.blank?
 
     # Is there a pid, and is it an admin set?
 
@@ -11,5 +12,8 @@ class Users::InvitationsController < Devise::InvitationsController
     # Are they valid (basic regex)?
 
     # Loop and process
+  else # must be logged in to mass invite
+      render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false and return
+    end
   end
 end
