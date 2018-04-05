6e942ef3ead739c185b17ae5c561ca5b1f3adc95
David Cliff <dgcliff@northeastern.edu>
2018-04-05 10:11:51 -0400

    Env fix

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Charon
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.web_console.whitelisted_ips = '10.0.2.2'
+    if Rails.env.development?
+      config.web_console.whitelisted_ips = '10.0.2.2'
+    end
   end
 end
