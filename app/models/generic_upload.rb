8b47412839af59093ee83e6a4df3fa3726e14b64
David Cliff <dgcliff@northeastern.edu>
2018-06-18 12:11:48 -0400

    Copy pasta fix

--- app/models/generic_upload.rb
+++ app/models/generic_upload.rb
@@ -1,4 +1,4 @@
 class GenericUpload < ApplicationRecord
   mount_uploaders :generics, GenericUploader
-  serialize :avatars, JSON # If you use SQLite, add this line.
+  serialize :generics, JSON # If you use SQLite, add this line.
 end
