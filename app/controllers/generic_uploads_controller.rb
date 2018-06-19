477fd5b4a215bacb95b3d3c379841f5307148781
David Cliff <dgcliff@northeastern.edu>
2018-06-19 15:55:57 -0400

    Basic generic uploading now working with Carrierwave

--- app/controllers/generic_uploads_controller.rb
+++ app/controllers/generic_uploads_controller.rb
@@ -2,4 +2,15 @@ class GenericUploadsController < ApplicationController
   def new
     @generic_upload = GenericUpload.new
   end
+
+  def create
+    puts params.inspect
+
+    gu = GenericUpload.new(params[:generic_upload].permit({generics: []}))
+    gu.save!
+
+    puts gu.generics[0].url # => '/url/to/file.png'
+    puts gu.generics[0].current_path # => 'path/to/file.png'
+    puts gu.generics[0].identifier # => 'file.png'
+  end
 end
