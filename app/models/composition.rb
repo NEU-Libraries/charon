62e3053b992936d241635ec6fddb26ece1217daf
David Cliff <dgcliff@northeastern.edu>
2018-06-22 15:16:27 -0400

    Clean up. Ordered members, members,  member ids and filters are a confusing setup...

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -3,8 +3,6 @@
 class Composition < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
 
-  filters_association :members, as: :pages, condition: :page?
-
   self.indexer = CompositionIndexer
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
@@ -13,8 +11,4 @@ class Composition < ActiveFedora::Base
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
-
-  def page?
-    false
-  end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -6,8 +6,4 @@ class Page < FileSet
   property :text, predicate: ::RDF::URI.new('http://opaquenamespace.org/hydra/pageText'), multiple: false do |index|
     index.as :stored_searchable
   end
-
-  def page?
-    true
-  end
 end
