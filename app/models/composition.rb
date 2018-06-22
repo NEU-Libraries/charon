34835b0ecaa2996affa69e54617b93ee50776282
David Cliff <dgcliff@northeastern.edu>
2018-06-22 13:46:56 -0400

    Adding file set features

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -3,6 +3,8 @@
 class Composition < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
 
+  filters_association :members, as: :pages, condition: :page?
+
   self.indexer = CompositionIndexer
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
@@ -13,4 +15,8 @@ class Composition < ActiveFedora::Base
   include ::Hyrax::BasicMetadata
 
   has_many :pages
+
+  def page?
+    false
+  end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,19 @@
 class Page < ActiveFedora::Base
   include Hydra::AccessControls::Permissions
   include Hydra::PCDM::ObjectBehavior
+  include Hydra::Works::FileSetBehavior
+
+  property :page_number, predicate: ::RDF::URI.new('http://opaquenamespace.org/hydra/pageNumber'), multiple: false do |index|
+    index.as :stored_searchable
+    index.type :integer
+  end
+  property :text, predicate: ::RDF::URI.new('http://opaquenamespace.org/hydra/pageText'), multiple: false do |index|
+    index.as :stored_searchable
+  end
 
   belongs_to :composition, :class_name => "Composition", predicate: ActiveFedora::RDF::Fcrepo::RelsExt.isPartOf
+
+  def page?
+    true
+  end
 end
