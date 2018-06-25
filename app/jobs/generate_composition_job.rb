e842682cf04d695fa8f8ac5cc8a5da88d1bc1c66
David Cliff <dgcliff@northeastern.edu>
2018-06-25 15:21:20 -0400

    Repurposing libera command line processing for job

--- app/jobs/generate_composition_job.rb
+++ app/jobs/generate_composition_job.rb
@@ -1,7 +1,39 @@
 class GenerateCompositionJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(pdf_location, working_dir)
+    parser = Libera::Parser.new
+
+    file_list = []
+
+    reader = PDF::Reader.new(pdf_location)
+    page_count = reader.page_count - 1
+
+    progressbar = ProgressBar.create(:title => "Pages", :starting_at => 0, :total => page_count + 1)
+
+    page_list = Hash.new
+
+    for i in 0..page_count
+      begin
+        pdf = Magick::ImageList.new(pdf_location + "[#{i}]") {self.density = Libera.configuration.density; self.quality = Libera.configuration.quality}
+        page_img = pdf.first
+
+        page_img.border!(0, 0, 'white')
+        page_img.alpha(Magick::DeactivateAlphaChannel)
+
+        file_name = "pdf-page-#{i}.#{Libera.configuration.format_type}"
+        file_path = "#{working_dir}/#{file_name}"
+
+        file_list << file_path
+        page_img.write(file_path) {self.depth = 8}
+
+        txt = parser.parse_image(file_path, i)
+        page_list[file_name] = txt
+      ensure
+        pdf.destroy! && page_img.destroy!
+      end
+    end
+
+    parser.generate_tei(page_list)
   end
 end
