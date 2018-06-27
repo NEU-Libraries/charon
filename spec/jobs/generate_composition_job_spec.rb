619ea7672abc2140ee4f6aafbbe514342e4d45f6
David Cliff <dgcliff@northeastern.edu>
2018-06-27 14:45:46 -0400

    Fixture file and dir fixes

--- spec/jobs/generate_composition_job_spec.rb
+++ spec/jobs/generate_composition_job_spec.rb
@@ -4,7 +4,15 @@ RSpec.describe GenerateCompositionJob, type: :job do
   include ActiveJob::TestHelper
 
   let(:composition) { Composition.create(:title => ["Test Composition"]) }
-  subject(:job) { described_class.perform_later(composition.id, "", "") }
+  subject(:job) { described_class.perform_later(composition.id, "#{Rails.root}/spec/fixtures/files/test.pdf", "#{Rails.root}/tmp/GenerateCompositionJobTest") }
+
+  before(:all) do
+    Libera.configuration.working_dir = "#{Rails.root}/tmp/GenerateCompositionJobTest"
+    if File.directory?(Libera.configuration.working_dir)
+      FileUtils.rm_rf(Libera.configuration.working_dir)
+    end
+    FileUtils.mkdir(Libera.configuration.working_dir)
+  end
 
   it "matches with enqueued job" do
     ActiveJob::Base.queue_adapter = :test
@@ -19,5 +27,7 @@ RSpec.describe GenerateCompositionJob, type: :job do
 
   it "creates pages" do
     perform_enqueued_jobs { job }
+    puts "DGC DEBUG"
+    puts composition.inspect
   end
 end
