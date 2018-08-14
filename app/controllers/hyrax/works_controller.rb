c8f309656344279c991ac733cf53319696b3171c
David Cliff <dgcliff@northeastern.edu>
2018-08-14 14:41:51 -0400

    Testing and stubbing out XML response format for CWRC writer

--- app/controllers/hyrax/works_controller.rb
+++ app/controllers/hyrax/works_controller.rb
@@ -10,5 +10,13 @@ module Hyrax
 
     # Use this line if you want to use a custom presenter
     self.show_presenter = Hyrax::WorkPresenter
+
+    def additional_response_formats(format)
+      format.xml do
+          send_data(show_presenter.export_as_xml,
+                    type: "text/xml",
+                    filename: show_presenter.xml_filename)
+      end
+    end
   end
 end
