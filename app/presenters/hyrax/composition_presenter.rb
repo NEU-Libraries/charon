e9a19a077fd73c86f14651a8e9f7225eca70408c
David Cliff <dgcliff@northeastern.edu>
2018-08-14 15:08:45 -0400

    Move to the proper work type - compositions

--- app/presenters/hyrax/composition_presenter.rb
+++ app/presenters/hyrax/composition_presenter.rb
@@ -2,5 +2,12 @@
 #  `rails generate hyrax:work Composition`
 module Hyrax
   class CompositionPresenter < Hyrax::WorkShowPresenter
+    def export_as_xml
+      "<TEI xmlns=\"http://www.tei-c.org/ns/1.0\"><teiHeader><fileDesc><titleStmt><title/></titleStmt><publicationStmt><p/></publicationStmt><sourceDesc><p/></sourceDesc></fileDesc></teiHeader><text><body><ab>And God said, Let there be light: and there was light.</ab></body></text></TEI>"
+    end
+
+    def xml_filename
+      "derp.xml"
+    end
   end
 end
