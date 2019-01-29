1ffb66f8a078b6db17ff9879ffd02d3760ef5cf5
David Cliff <d.cliff@neu.edu>
2019-01-29 11:08:11 -0500

    Stubbing out change set specs

---
+++ spec/change_sets/project_change_set_spec.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+require "valkyrie/specs/shared_specs"
+
+describe ProjectChangeSet do
+end
---
+++ spec/change_sets/work_change_set_spec.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+require "valkyrie/specs/shared_specs"
+
+describe WorkChangeSet do
+end
