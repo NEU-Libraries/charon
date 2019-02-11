c242c62b1a35981c2e1c6a17574577d4f03f23a6
David Cliff <d.cliff@neu.edu>
2019-02-11 12:18:13 -0500

    Better git log, bootstrap table. Just need to make git SHAs links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   layout "application"
   def home
-    @git_log = `cd #{Rails.root} && git log -n 20 --decorate --oneline`
+    raw = `cd #{Rails.root} && git log -n 20 --pretty="format:%h|%s"`
+    @git_log = Hash.new
+    raw.split("\n").each do |line|
+      hsh_and_val = line.split('|', 2)
+      @git_log[hsh_and_val[0]] = hsh_and_val[1]
+    end
   end
 end
