40256934dd7139ff5d85b21aafd63e577acc55f0
David Cliff <d.cliff@neu.edu>
2019-03-04 12:02:29 -0500

    Full search state coverage

---
+++ spec/concerns/search_state_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe SearchState do
+  let(:params) { ActionController::Parameters.new }
+  let(:config) { Blacklight::Configuration.new }
+  let(:controller) { double }
+  let(:search_state) { described_class.new(params, config, controller) }
+  let(:human_readable_doc) { SolrDocument.new(id: '1', human_readable_type_ssim: 'Project') }
+
+  describe '#url_for_document defaults to doc' do
+    subject { search_state.url_for_document(SolrDocument.new) }
+    it { is_expected.to be_kind_of SolrDocument }
+  end
+
+  describe '#url_for_document reacts to human readable type' do
+    subject { search_state.url_for_document(human_readable_doc) }
+    it { is_expected.to eq('/projects/1') }
+  end
+end
