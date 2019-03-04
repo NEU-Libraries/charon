408488281fc40f7817aa41f3c61b652025c20c42
David Cliff <d.cliff@neu.edu>
2019-03-04 13:21:49 -0500

    Stubbing out controller specs and rubocop pass

--- spec/concerns/searchable_spec.rb
+++ spec/concerns/searchable_spec.rb
@@ -3,7 +3,6 @@
 require 'spec_helper'
 
 describe Searchable do
-
   before do
     class FakesController < ApplicationController
       include Searchable
@@ -13,11 +12,9 @@ describe Searchable do
   let(:object) { FakesController.new }
 
   describe '#searchable' do
-
     it 'is searchable' do
       object.searchable
       expect(object.instance_variable_get(:@searchable)).to be true
     end
-
   end
 end
