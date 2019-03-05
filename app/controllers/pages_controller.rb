5bb2921dd93090d9166db856fd01bd1e9f00de5f
David Cliff <d.cliff@neu.edu>
2019-03-05 13:18:33 -0500

    Adding coverage skip for dev git list on front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,13 @@ class PagesController < ApplicationController
   before_action :searchable, only: [:home]
 
   def home
+    # :nocov:
     raw = `cd #{Rails.root} && git log -n 20 --pretty="format:%h|%s"`
     @git_log = {}
     raw.split("\n").each do |line|
       hsh_and_val = line.split('|', 2)
       @git_log[hsh_and_val[0]] = hsh_and_val[1]
     end
+    # :nocov:
   end
 end
