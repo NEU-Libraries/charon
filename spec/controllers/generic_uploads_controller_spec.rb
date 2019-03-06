959a2bd37e00f466ad594e1b769eef15ee2672f0
David Cliff <dgcliff@northeastern.edu>
2019-03-05 21:53:49 -0500

    Controller finished

--- spec/controllers/generic_uploads_controller_spec.rb
+++ spec/controllers/generic_uploads_controller_spec.rb
@@ -16,4 +16,13 @@ describe GenericUploadsController do
       expect(response).to render_template('generic_uploads/index')
     end
   end
+
+  describe "create" do
+    context "with valid input" do
+      it "creates a generic upload" do
+        file = fixture_file_upload(Rails.root.join('public', 'apple-touch-icon.png'), 'image/png')
+        expect { post :create, params: { generic_upload: { binary: file } } }.to change(ActiveStorage::Attachment, :count).by(1)
+      end
+    end
+  end
 end
