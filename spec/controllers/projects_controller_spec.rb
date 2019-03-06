10e09ed58b2d5389338b068326e27a0e6b7f0389
David Cliff <dgcliff@northeastern.edu>
2019-03-05 22:00:26 -0500

    True 100% coverage!

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,4 +3,19 @@
 require 'rails_helper'
 
 describe ProjectsController do
+  describe 'new' do
+    it 'renders the new record form' do
+      get :new
+      expect(response).to render_template('projects/new')
+    end
+  end
+
+  describe "create" do
+    context "with valid input" do
+      it "creates a project" do
+        post :create, params: { project: { title: 'Cat', description: 'In the hat' } }
+        expect(assigns(:project).title).to eq('Cat')
+      end
+    end
+  end
 end
