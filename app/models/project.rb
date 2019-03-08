23f8c3bc033268ebb871f1d19d48ad560800353b
David Cliff <d.cliff@neu.edu>
2019-03-08 13:19:25 -0500

    Update project to use registry id not user id list

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,5 +5,5 @@ class Project < Valkyrie::Resource
   include Valkyrie::Resource::AccessControls
   attribute :title, Valkyrie::Types::String
   attribute :description, Valkyrie::Types::String
-  attribute :user_ids, Valkyrie::Types::Array
+  attribute :user_registry_id, Valkyrie::Types::Integer
 end
