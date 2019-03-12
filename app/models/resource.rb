b06cb1e1bcdf69766bc84818f8492fdeabcff703
David Cliff <d.cliff@neu.edu>
2019-03-11 22:15:04 -0400

    Rubocop clean and lint adjustment for dry types expectations

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Resource < Valkyrie::Resource
-  attribute :alternate_ids, Valkyrie::Types::Set.of(Valkyrie::Types::ID).meta(ordered: true).default{[::Noid::Rails::Service.new.mint]}
+  attribute :alternate_ids,
+            Valkyrie::Types::Set.of(Valkyrie::Types::ID).meta(ordered: true).default {
+              [::Noid::Rails::Service.new.mint]
+            }
 
   def to_param
     alternate_ids.first.to_s
