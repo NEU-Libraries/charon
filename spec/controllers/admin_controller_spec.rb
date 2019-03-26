1b2096cb94a611016831a590ad0e1fead04cb03d
David Cliff <d.cliff@neu.edu>
2019-03-26 11:38:53 -0400

    Spec all up to date

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -15,4 +15,22 @@ describe AdminController do
       expect(response.status).to eq(403)
     end
   end
+
+  describe 'new_user' do
+    render_views
+    it 'renders new user form' do
+      sign_in FactoryBot.create(:admin)
+      get :new_user
+      expect(response.body).to include('Create a user')
+    end
+  end
+
+  describe 'create_user' do
+    it 'creates a user and emails them a notifications' do
+      sign_in FactoryBot.create(:admin)
+      post :create_user, {:params => {:user => {:email => 'test@email.com', :first_name => 'Doug', :last_name => 'Dimmadome'}}}
+      mail = ActionMailer::Base.deliveries.last
+      expect(mail['to'].to_s).to eq('test@email.com')
+    end
+  end
 end
