d629665500868d232946f41652d78f0994e30066
David Cliff <d.cliff@neu.edu>
2019-03-29 15:54:50 -0400

    Gating task partials behind role designations

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,24 @@ class Role < ApplicationRecord
   enumeration :designation
   belongs_to :user_registry
   belongs_to :user
+
+  def manager?
+    user.admin? || (designation == :manager)
+  end
+
+  def editor?
+    user.admin? || (designation == :manager) || (designation == :editor)
+  end
+
+  def creator?
+    user.admin? || (designation == :manager) || (designation == :editor) || (designation == :creator)
+  end
+
+  def depositor?
+    user.admin?                   ||
+      (designation == :manager)   ||
+      (designation == :editor)    ||
+      (designation == :creator)   ||
+      (designation == :depositor)
+  end
 end
