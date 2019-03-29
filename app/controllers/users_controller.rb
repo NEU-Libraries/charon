ad221eacfce1e1cc2be51a066d162d0e9a3e8cfb
David Cliff <d.cliff@neu.edu>
2019-03-29 15:24:18 -0400

    Finding and tying in role into task list partial. Table instead of cards. Color coding tasks.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def tasks
     meta = Valkyrie::MetadataAdapter.find(:composite_persister)
     @project = meta.query_service.find_by_alternate_identifier(alternate_identifier: params[:project_id])
+    @role = Role.find_by(user_id: current_user.id, user_registry_id: @project.user_registry_id)
   end
 
   def dashboard; end
