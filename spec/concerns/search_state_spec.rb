bba7379f161b03be12af22144ed154a25bdf4335
David Cliff <dgcliff@northeastern.edu>
2019-05-09 15:19:20 -0400

    Fix search state spec

--- spec/concerns/search_state_spec.rb
+++ spec/concerns/search_state_spec.rb
@@ -8,15 +8,15 @@ describe SearchState do
   let(:controller) { double }
   let(:search_state) { described_class.new(params, config, controller) }
   let(:noid) { Minter.mint }
-  let(:human_readable_doc) { SolrDocument.new(id: '1', human_readable_type_ssim: ['Project'], alternate_ids_tesim: [noid]) }
+  let(:doc) { SolrDocument.new(id: '1', internal_resource_tesim: ['Project'], alternate_ids_tesim: [noid]) }
 
   describe '#url_for_document defaults to doc' do
     subject { search_state.url_for_document(SolrDocument.new) }
     it { is_expected.to be_kind_of SolrDocument }
   end
 
-  describe '#url_for_document reacts to human readable type' do
-    subject { search_state.url_for_document(human_readable_doc) }
+  describe '#url_for_document reacts to klass' do
+    subject { search_state.url_for_document(doc) }
     it { is_expected.to eq("/projects/#{noid}") }
   end
 end
