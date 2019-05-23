fe9ac9012b03555c8c85c41b4d862ef12e3ea12a
David Cliff <dgcliff@northeastern.edu>
2019-05-23 12:01:07 -0400

    Multi-select for adding multiple users to project. Need to wire it up with a form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -33,8 +33,8 @@ class ProjectsController < ApplicationController
   def available_users
     # Need to filter down to users not already attached to project
     meta = Valkyrie::MetadataAdapter.find(:composite_persister)
-    project = meta.query_service.find_by_alternate_identifier(alternate_identifier: params[:project_id])
-    already_attached_users = project.users
+    @project = meta.query_service.find_by_alternate_identifier(alternate_identifier: params[:project_id])
+    already_attached_users = @project.users
     @users = User.all.select{|u| !already_attached_users.include? u}
   end
 
