2f56b391bb7d18abd99d3a8f6a969891e759c78c
David Cliff <dgcliff@northeastern.edu>
2019-05-23 09:36:23 -0400

    Remove web console config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,8 +4,6 @@ Rails.application.configure do
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.web_console.whitelisted_ips = '172.18.0.1'
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
