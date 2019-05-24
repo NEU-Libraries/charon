87bf85755a08d48e45319ad93cd59d8965e53ddc
David Cliff <dgcliff@northeastern.edu>
2019-05-24 12:03:11 -0400

    Update roles controller spec

---
+++ spec/controllers/roles_controller_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe RolesController do
+  let(:role) { create(:role) }
+
+  describe 'edit' do
+    it 'renders the partial' do
+      get :edit, params: { id: role.id }
+      expect(response).to render_template('roles/edit')
+    end
+  end
+
+  describe 'update' do
+    it 'modifies the designation' do
+      expect(role.designation).to eq Designation.user
+      post :update, params: { id: role.id, designation: 'manager' }
+      expect(role.reload.designation).to eq Designation.manager
+    end
+  end
+end
--- spec/factories/role.rb
+++ spec/factories/role.rb
@@ -3,5 +3,7 @@
 FactoryBot.define do
   factory :role do
     designation { Designation.user }
+    user { create(:user) }
+    user_registry { FactoryBot.create_for_repository(:project).user_registry }
   end
 end
