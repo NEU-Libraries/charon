3640947777a242ee6f2c92db32392e31214580ee
David Cliff <dgcliff@northeastern.edu>
2019-06-06 11:38:19 -0400

    Fleshing out breadcrumbs for dashboard paths. Would like to put breadcrumbs in by default. Will require copypasta of Blacklight layout though

--- app/models/breadcrumb_trail.rb
+++ app/models/breadcrumb_trail.rb
@@ -5,4 +5,10 @@ class BreadcrumbTrail < Croutons::BreadcrumbTrail
     breadcrumb(objects[:project].title, actions_path(objects[:project]))
     breadcrumb('User Registry')
   end
+
+  def roles_edit
+    breadcrumb(objects[:role].project.title, actions_path(objects[:role].project))
+    breadcrumb('User Registry', project_users_path(objects[:role].project))
+    breadcrumb(objects[:role].user.first_name + '\'s Role')
+  end
 end
