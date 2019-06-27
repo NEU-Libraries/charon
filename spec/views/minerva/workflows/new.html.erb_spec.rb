5f05661b5065056f6cf00bb79eee4357abb94eef
David Cliff <dgcliff@northeastern.edu>
2019-06-27 14:36:54 -0400

    Rubocop fix

--- spec/views/minerva/workflows/new.html.erb_spec.rb
+++ spec/views/minerva/workflows/new.html.erb_spec.rb
@@ -2,9 +2,9 @@
 
 require 'spec_helper'
 
-describe "minerva/workflows/new.html.erb" do
-  it "appropiately raises a no method error with bad pathing" do
-    stub_template "minerva/workflows/new.html.erb" => "<%= bad_url_path %><br/>"
+describe 'minerva/workflows/new.html.erb' do
+  it 'appropiately raises a no method error with bad pathing' do
+    stub_template 'minerva/workflows/new.html.erb' => '<%= bad_url_path %><br/>'
     expect { render }.to raise_error(ActionView::Template::Error)
   end
 end
