67b8703882a4503911f6288c551dbbbf172f6a5c
David Cliff <dgcliff@northeastern.edu>
2019-07-11 11:41:40 -0400

    Using wait-for-it as depends_on isn't supported in swarm mode

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-
+./scripts/wait-for-it.sh -t 60 db:5432 --strict -- echo "db is up"
+./scripts/wait-for-it.sh -t 60 solr:8983 --strict -- echo "solr is up"
 rm -f /home/charon/web/tmp/pids/server.pid
 rake db:create
 rake db:migrate
