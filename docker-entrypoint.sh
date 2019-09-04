ad5b7ef14958c32dd6842c061d3279ba9d7e905e
David Cliff <dgcliff@northeastern.edu>
2019-09-04 16:41:40 -0400

    Take migration out of entry point, change CI to test env and add in migration there

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,4 @@ sleep 10
 dockerize -wait tcp://db:5432 -timeout 1m
 dockerize -wait http://solr:8983 -timeout 1m
 rm -f /home/charon/web/tmp/pids/server.pid
-rake db:create
-rake db:migrate
-rake db:migrate RAILS_ENV=test
 rails s -p 3000 -b '0.0.0.0'
