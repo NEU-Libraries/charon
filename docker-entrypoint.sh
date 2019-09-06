0edd6e0f7e940a7df366f38a0b846883c1ba4950
David Cliff <dgcliff@northeastern.edu>
2019-09-05 23:12:08 -0400

    Enable staging env caching

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,4 @@ set -e
 dockerize -wait tcp://db:5432 -timeout 1m
 dockerize -wait http://solr:8983 -timeout 1m
 rm -f /home/charon/web/tmp/pids/server.pid
-rails dev:cache
 rails s -p 3000 -b '0.0.0.0'
