24efce9cd9643c21b6f625aec15eaa98403f4133
David Cliff <dgcliff@northeastern.edu>
2019-09-26 13:53:05 -0400

    Clean up and connecting manager action partial

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -2,22 +2,15 @@
 
 class WorkflowsController < ApplicationController
   def new
-    # @project_id = params[:project_id]
-    # @creator_id = params[:creator_id]
-    @project_id = Minerva::Project.find_or_create_by(:auid => '3tx969p').id
-    @creator_id = Minerva::User.find_or_create_by(:auid => '28').id
+    @project_id = Minerva::Project.find_or_create_by(:auid => params[:project_id]).id
+    @creator_id = Minerva::User.find_or_create_by(:auid => current_user.id).id
     @workflow = Minerva::Workflow.new
     @tasks = Task.all
   end
 
   def create
-    # flash[:notice] = params[:workflow]["title"]
-    # flash[:notice] = params[:workflow].inspect
     permitted = params.require(:workflow).permit(:title, :ordered, :task_list, :project_id, :creator_id)
-    # flash[:notice] = (params.require(:workflow).permit(:title, :ordered, :task_list)).inspect
     @workflow = Minerva::Workflow.create!(permitted)
-    # flash[:notice] = @workflow.inspect
-    # redirect_to root_path
     redirect_to workflow_path(@workflow)
   end
 
