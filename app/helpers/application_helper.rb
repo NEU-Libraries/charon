8e6596ecaab030d585d08d913bcbcbe8500f383c
David Cliff <dgcliff@northeastern.edu>
2019-09-30 16:53:16 -0400

    By committing to removing minvera's engine capacity we can remove the method missing kludge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  # kludge so we can keep Minerva as an isolated namespace but still use Blacklight layout
-  def method_missing(method, *args, &block)
-    if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
-      main_app.send(method, *args)
-    else
-      super
-    end
-  end
-
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
