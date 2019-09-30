423c11b69e82a9e27d5ad75bb824beab3b06ba13
David Cliff <dgcliff@northeastern.edu>
2019-09-30 19:25:10 -0400

    Workflow controller done

--- spec/controllers/workflows_controller_spec.rb
+++ spec/controllers/workflows_controller_spec.rb
@@ -16,6 +16,16 @@ describe WorkflowsController do
     end
   end
 
+  describe 'create' do
+    it 'creates a workflow' do
+      sign_in admin_user
+      pid = Minerva::Project.find_or_create_by(auid: project.noid).id
+      cid = Minerva::User.find_or_create_by(auid: admin_user.id).id
+      post :create, params: { workflow: { project_id: pid, creator_id: cid, title: 'Test', ordered: true, task_list: "[\"Encode\",\"Catalog\",\"Review\",\"Transcribe\",\"Publish\"]" } }
+      response.should redirect_to workflow_path(Workflow.last)
+    end
+  end
+
   describe 'show' do
     render_views
     it 'renders the show view' do
@@ -30,6 +40,16 @@ describe WorkflowsController do
       sign_in admin_user
       get :edit, params: { id: workflow.id }
       expect(CGI.unescapeHTML(response.body)).to include(workflow.title)
+    end
+  end
+
+  describe 'update' do
+    it 'udpates a workflow' do
+      sign_in admin_user
+      pid = Minerva::Project.find_or_create_by(auid: project.noid).id
+      cid = Minerva::User.find_or_create_by(auid: admin_user.id).id
+      post :update, params: { id: workflow.id, workflow: { project_id: pid, creator_id: cid, title: 'Test', ordered: true, task_list: "[\"Encode\",\"Catalog\",\"Review\",\"Transcribe\",\"Publish\"]" } }
+      response.should redirect_to workflow_path(workflow.id)
     end
   end
 end
--- spec/factories/workflow.rb
+++ spec/factories/workflow.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :workflow do
     title      { Faker::Company.name }
-    project_id { Minerva::Project.create(auid: FactoryBot.create_for_repository(:project).id).id }
+    project_id { Minerva::Project.create(auid: FactoryBot.create_for_repository(:project).noid).id }
     creator_id { Minerva::User.create(auid: create(:user).id).id }
     task_list  { Task.all.map{ |t| t.name}.to_s }
     ordered    { true }
