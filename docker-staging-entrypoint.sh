5a0a06cda423e1c9c5f49d858225ca7a7ca33abd
David Cliff <dgcliff@northeastern.edu>
2019-10-11 12:48:59 -0400

    Updating some scripts

--- docker-staging-entrypoint.sh
+++ docker-staging-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -e
 dockerize -wait tcp://db:5432 -timeout 1m
 dockerize -wait http://solr:8983 -timeout 1m
 rm -f /home/charon/web/tmp/pids/server.pid
-rails db:migrate
+rails db:migrate 2>/dev/null
 rails s -p 3000 -b '0.0.0.0'
