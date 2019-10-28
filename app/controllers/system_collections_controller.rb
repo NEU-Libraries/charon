74e8aa3520092d80d271d5477209da91338b90b7
David Cliff <dgcliff@northeastern.edu>
2019-10-28 12:20:14 -0400

    Blacklight integration for system collection controller

--- app/controllers/system_collections_controller.rb
+++ app/controllers/system_collections_controller.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
-class SystemCollectionsController < ApplicationController
+class SystemCollectionsController < CatalogController
   include Searchable
 
   load_resource except: %i[new create edit update]
   before_action :searchable, only: [:show]
+
+  helper_method :sort_column, :sort_direction
+
+  # Blacklight incantations
+  blacklight_config.track_search_session = false
+  layout 'application'
 
   def new; end
 
@@ -14,5 +20,11 @@ class SystemCollectionsController < ApplicationController
 
   def update; end
 
-  def show; end
+  def show
+    @response, @document_list = search_service.fetch(
+      metadata_adapter.query_service.find_inverse_references_by(
+        resource: @system_collection, property: :a_member_of
+      ).map(&:id).map(&:to_s).to_a
+    )
+  end
 end
