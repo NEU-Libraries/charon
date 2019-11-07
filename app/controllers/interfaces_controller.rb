1cb66846a2bda9a8a23e8024fbab93709cd8483e
David Cliff <dgcliff@northeastern.edu>
2019-11-07 13:05:15 -0500

    Stubbing out interfaces

---
+++ app/controllers/interfaces_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class InterfacesController < ApplicationController
+  def new; end
+
+  def create; end
+
+  def edit; end
+
+  def update; end
+
+  def show; end
+
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,7 @@ Rails.application.routes.draw do
   resources :roles
   resources :workflows
   resources :works
+  resources :interfaces
 
   root to: 'pages#home'
 
