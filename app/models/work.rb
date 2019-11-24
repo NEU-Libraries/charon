176d1a250e84e6b6ce1df73e4e8f7cb5806e0698
David Cliff <dgcliff@northeastern.edu>
2019-11-24 13:45:10 -0500

    Mods and HTML converted fields for Works

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,4 +4,6 @@ class Work < Resource
   attribute :title, Valkyrie::Types::String
   attribute :a_member_of, Valkyrie::Types::Set.of(Valkyrie::Types::ID).meta(ordered: true)
   attribute :workflow_id, Valkyrie::Types::Integer
+  attribute :mods, Valkyrie::Types::String
+  attribute :mods_html, Valkyrie::Types::String
 end
