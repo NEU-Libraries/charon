f2e5861586092c82f7834d385305e47769a1a2b6
David Cliff <dgcliff@northeastern.edu>
2019-11-25 14:56:09 -0500

    Connected. Will need to keep an eye on performance

--- app/helpers/interfaces_helper.rb
+++ app/helpers/interfaces_helper.rb
@@ -4,12 +4,4 @@ module InterfacesHelper
   def upload_interface
     Minerva::Interface.find_by(title: "upload")
   end
-
-  def interface_past_tense(interface)
-    I18n.t "charon.interface.#{interface}.past_tense"
-  end
-
-  def interface_present_tense(interface)
-    I18n.t "charon.interface.#{interface}.present_tense"
-  end
 end
--- app/models/interface.rb
+++ app/models/interface.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
-class Interface < Minerva::Workflow
+class Interface < Minerva::Interface
+  def past_tense
+    I18n.t "charon.interface.#{title}.past_tense"
+  end
+
+  def present_tense(interface)
+    I18n.t "charon.interface.#{title}.present_tense"
+  end
 end
