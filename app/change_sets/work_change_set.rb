a7031a702ca1568b98318f25cb36f0fc34440862
David Cliff <dgcliff@northeastern.edu>
2019-11-27 12:31:10 -0500

    Stubbing out catalog update mechanism

--- app/change_sets/work_change_set.rb
+++ app/change_sets/work_change_set.rb
@@ -3,5 +3,6 @@
 class WorkChangeSet < Valkyrie::ChangeSet
   property :title
   property :a_member_of
+  property :mods_xml
   validates :title, presence: true
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,5 +2,12 @@
 
 class TasksController < ApplicationController
   def catalog
+    work = Work.find(params[:id])
+    # raw xml param
+    change_set = WorkChangeSet.new(work)
+    if change_set.validate(params[:work])
+      change_set.sync
+      work = metadata_adapter.persister.save(resource: change_set.resource)
+    end
   end
 end
