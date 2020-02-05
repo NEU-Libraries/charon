02b47c6219e3db9b8c0a37620242877501e9a408
David Cliff <dgcliff@northeastern.edu>
2020-02-05 13:37:22 -0500

    Fixes #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   include InterfacesHelper
   include MinervaHelper
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Best to ask for breadcrumbs everywhere
   # and just avoid Croutons NotImplementedError
   def breadcrumbs
@@ -43,4 +45,10 @@ class ApplicationController < ActionController::Base
     render_401 && return unless current_user
     render_403 && return unless current_user.admin?
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    end
 end
