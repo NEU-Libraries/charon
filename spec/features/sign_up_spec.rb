0517cc15f6c8be88da36da7a51bdc9132285cec7
David Cliff <dgcliff@northeastern.edu>
2020-02-05 13:57:15 -0500

    Spec fix

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -24,8 +24,8 @@ feature 'Visitor signs up' do
   def sign_up_with(email, password)
     visit new_user_registration_path
     fill_in 'Email', with: email
-    fill_in 'Password', with: password
-    fill_in 'Password confirmation', with: password
+    fill_in 'user_password', with: password
+    fill_in 'user_password_confirmation', with: password
     click_button 'Sign up'
   end
 end
