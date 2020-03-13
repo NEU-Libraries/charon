fc31c0e041cc780d6e2011725b3fe8b950f22734
David Cliff <dgcliff@northeastern.edu>
2020-03-13 10:58:57 -0400

    Using iiif and config_for to setup IIIF url value. Need to setup proxypass on staging for value to work

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module Charon
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.iiif = config_for(:iiif)["url"]
   end
 end
