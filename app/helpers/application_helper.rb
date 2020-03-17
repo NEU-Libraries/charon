811c732507bd3d43a49584f0a4ffe14a29d16e29
David Cliff <dgcliff@northeastern.edu>
2020-03-17 11:12:49 -0400

    Better. Will re-evaluate once it's gone through some pacing on staging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,19 +19,10 @@ module ApplicationHelper
   end
 
   def render_thumbnail(document, _options)
-    # image_tag('https://repository.library.northeastern.edu/downloads/neu:m0451f929?datastream_id=thumbnail_4', size: '50x50')
-    # if document.thumbnail?
-    #   # Need to pass in an array to account for 2x values
-    #   @large_thumbnail_path   = "#{iiif_url}/2/#{document.id}.jp2/full/!400,400/0/default.jpg"
-    #   @medium_thumbnail_path  = "#{iiif_url}/2/#{document.id}.jp2/full/!200,200/0/default.jpg"
-    #   @small_thumbnail_path   = "#{iiif_url}/2/#{document.id}.jp2/full/!100,100/0/default.jpg"
-    #
-    # end
-    render partial: 'shared/thumbnail', :locals => {:document => document}
+    render partial: 'shared/thumbnail', :locals => {:document => document, :iiif => iiif_url}
   end
 
-  def iiif_thumbnail_url(id, size)
-    base_url = Rails.application.config.iiif['url']
-    return "#{base_url}/2/#{id}.jp2/full/!#{size},#{size}/0/default.jpg"
+  def iiif_url
+    Rails.application.config.iiif['url']
   end
 end
