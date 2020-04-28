d1d4d62700c5a98611a60fda539aad8d0011bfb9
David Cliff <dgcliff@northeastern.edu>
2020-04-28 13:02:47 -0400

    Full coverage. Still need to fix redirect.

--- spec/controllers/works_controller_spec.rb
+++ spec/controllers/works_controller_spec.rb
@@ -4,6 +4,29 @@ require 'rails_helper'
 
 describe WorksController do
   let(:work) { FactoryBot.create_for_repository(:work) }
+  let(:admin_user) { create(:admin) }
+
+  describe 'assign_task' do
+    it 'creates a new minerva task associated with interface and user' do
+      # TODO: update this when the redirect properly integrates with dashboard
+      sign_in admin_user
+      Minerva::Interface.create(title: 'transcribe', code_point: 'tasks#transcribe')
+      post :assign_task, params: { id: work.id, task: 'transcribe', user: { id: admin_user.id } }
+      expect(Minerva::State.count).to be 1
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  describe 'tasks' do
+    render_views
+    it 'lists tasks that can be assigned' do
+      # TODO: extend testing to verify users and tasks diplayed correlate with
+      # work, workflow and project
+      get :tasks, params: { id: work.noid }
+      expect(response).to render_template('works/tasks')
+      expect(CGI.unescapeHTML(response.body)).to include(work.title)
+    end
+  end
 
   describe 'show' do
     render_views
