009bef17b9f7dad511155e15379b495e32088d65
David Cliff <dgcliff@northeastern.edu>
2020-05-05 16:09:14 -0400

    Full test coverage

--- spec/factories/state.rb
+++ spec/factories/state.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
+include MinervaHelper
+
 FactoryBot.define do
   factory :state do
-    # title      { 'upload' }
-    # code_point { 'generic_uploads#new' }
+    transient do
+      work { FactoryBot.create_for_repository(:project) }
+    end
+
+    creator_id    { minerva_user_id(create(:admin).id) }
+    user_id       { minerva_user_id(create(:user).id) }
+    work_id       { minerva_work_id(work.noid) }
+    # TODO: make multiple interface factories and get rid of this one off
+    interface_id  { create(:interface, code_point: "tasks#encode").id }
+    status        { Status.available.name }
   end
 end
