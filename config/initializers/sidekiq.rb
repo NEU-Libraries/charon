f86bba28d60a9da8442956a3affcf66c9e4aa46d
David Cliff <dgcliff@northeastern.edu>
2020-06-15 14:55:35 -0400

    Lower redis verbosity and redis config change for deprecation notice

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,7 @@
 require 'sidekiq/web'
 
 Sidekiq::Web.set :sessions, false
+Redis.exists_returns_integer = true
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://redis:6379/0') }
