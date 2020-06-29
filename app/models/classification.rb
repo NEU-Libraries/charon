0780f63799779f846bcb4e326c49628e74edb490
David Cliff <dgcliff@northeastern.edu>
2020-06-29 15:59:04 -0400

    Stubbing out type enumeration from RADS excel

---
+++ app/models/classification.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Classification < Enumerations::Base
+  value :map,               name: 'Map'
+  value :dataset,           name: 'Dataset'
+  value :image,             name: 'Image'
+  value :video,             name: 'Video'
+  value :presentation,      name: 'Presentation'
+  value :audio,             name: 'Audio'
+  value :spreadsheet,       name: 'Spreadsheet'
+  value :text,              name: 'Text'
+  value :archive,           name: 'Archive'
+  value :musical_notation,  name: 'Musical Notation'
+end
