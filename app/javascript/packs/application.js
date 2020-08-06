bd0f16b330e12dfb6e999d67c40e72a7d967ab67
David Cliff <dgcliff@northeastern.edu>
2020-08-06 15:16:16 -0400

    Setting up jQuery with webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,8 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+// console.log('Hello World from Webpacker')
+
+$(function () {
+  console.log('Hello World from Webpacker');
+});
