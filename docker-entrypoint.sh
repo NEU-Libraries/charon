cf4309837fefe9781e4e04960d5049f3b74396ea
David Cliff <dgcliff@northeastern.edu>
2020-08-11 22:54:27 -0400

    entrypoint fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 dockerize -wait tcp://db:5432 -timeout 1m
 dockerize -wait http://solr:8983 -timeout 1m
 rm -f /home/charon/web/tmp/pids/server.pid
-rails s -p 3000 -b '0.0.0.0'
+bin/rails s -p 3000 -b '0.0.0.0'
