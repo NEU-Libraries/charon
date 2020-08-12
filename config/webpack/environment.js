94d71151d42e372201485c9893a6e5b41f545125
David Cliff <dgcliff@northeastern.edu>
2020-08-12 10:07:16 -0400

    Dont add packs on docker copy, and use chunks for webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,9 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
+
+environment.splitChunks()
+
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
