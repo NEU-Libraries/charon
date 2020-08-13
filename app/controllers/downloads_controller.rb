432d0d7eead347381345a24670e58200f5595010
David Cliff <dgcliff@northeastern.edu>
2020-08-13 15:05:34 -0400

    Button links working with download controller. Need labels and to hook up manifest

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class DownloadsController < ApplicationController
-  def download; end
+  def download
+    # take noid of blob and send file on file path
+    send_file Blob.find(params[:id]).file_path
+  end
 end
