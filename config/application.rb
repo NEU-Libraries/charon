6edda20bbe2bfe7d3b6c68d03f6381e6fd09fc49
David Cliff <dgcliff@northeastern.edu>
2020-09-11 10:40:34 -0400

    Hosts fix

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,7 @@ module Charon
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.hosts << "web"
-
+    config.hosts = []
     config.iiif = ActiveSupport::OrderedOptions.new
     config.iiif = config_for(:iiif)
   end
