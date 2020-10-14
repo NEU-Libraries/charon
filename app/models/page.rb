0cd93bb519311b80197fc4ab691ef2c9515dface
David Cliff <dgcliff@northeastern.edu>
2020-10-14 11:53:33 -0400

    Used some helper methods to keep the field named as is

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class Page < FileSet
-  attribute :text, Valkyrie::Types::Set.of(String)
+  attribute :versioned_text, Valkyrie::Types::Array
 
   def thumbnail
     files.select do |f|
       f.use == [Valkyrie::Vocab::PCDMUse.ThumbnailImage]
     end.first.noid
+  end
+
+  def text
+    versioned_text.last
+  end
+
+  def text=(text_value)
+    self.versioned_text += [text_value]
   end
 end
