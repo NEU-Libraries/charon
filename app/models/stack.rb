423a4e4965d4ba6b98c65a02d2dc75679d9cfdf8
David Cliff <dgcliff@northeastern.edu>
2020-10-14 12:01:55 -0400

    Add tei as attribute to Stack model. Need to refactor libera service to change tei persistence

--- app/models/stack.rb
+++ app/models/stack.rb
@@ -4,4 +4,5 @@ class Stack < Resource
   attribute :type, Valkyrie::Types::String
   attribute :member_ids, Valkyrie::Types::Set.of(Valkyrie::Types::ID).meta(ordered: true)
   attribute :a_member_of, Valkyrie::Types::ID
+  attribute :tei, Valkyrie::Types::String
 end
