43168b0f554dc28e0970e348314cd635f0d39c4d
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2021-03-24 14:06:07 -0400

    Image fix

--- docker-staging-entrypoint.sh
+++ docker-staging-entrypoint.sh
@@ -4,4 +4,5 @@ dockerize -wait http://solr:8983 -timeout 1m
 rm -f /home/charon/web/tmp/pids/server.pid
 rails db:migrate 2>/dev/null
 rails assets:precompile
+bundle exec rake leaflet:copy_images
 rails s -p 3000 -b '0.0.0.0'
---
+++ lib/tasks/leaflet.rake
@@ -0,0 +1,6 @@
+namespace :leaflet do
+  desc 'Copies binary assets to public for leaflet draw'
+  task copy_images: :environment do
+    FileUtils.cp_r 'node_modules/leaflet-draw/dist/images', 'public/assets'
+  end
+end
