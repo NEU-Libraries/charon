088e5d881bf2f31768eb7c94d8a1066b0bd77d4b
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2021-06-08 15:12:07 -0400

    created_at works for all active record objects

--- app/controllers/concerns/sortable.rb
+++ app/controllers/concerns/sortable.rb
@@ -4,9 +4,9 @@ module Sortable
   extend ActiveSupport::Concern
 
   def sort_column
-    return 'last_name' unless params[:sort]
+    return 'created_at' unless params[:sort]
 
-    (User.column_names + Role.column_names).include?(params[:sort].split('.').last) ? params[:sort] : 'last_name'
+    (User.column_names + Role.column_names).include?(params[:sort].split('.').last) ? params[:sort] : 'created_at'
   end
 
   def sort_direction
