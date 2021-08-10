331161cd01852f8ffd113433139f1f22ff6b9ddb
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2021-08-10 16:11:51 -0400

    Added user drop down for manager assignment at project creation - closes #17

--- app/change_sets/project_change_set.rb
+++ app/change_sets/project_change_set.rb
@@ -3,5 +3,10 @@
 class ProjectChangeSet < Valkyrie::ChangeSet
   property :title
   property :description
+  property :manager, virtual: true
   validates :title, presence: true
+
+  def manager=(manager_id)
+    resource.attach_user(User.find(manager_id), Designation.manager)
+  end
 end
