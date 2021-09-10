1e9f6e2c301f27d381272275220cbd8e3257eb42
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2021-09-10 11:08:55 -0400

    Making notes modular in anticipation of JS refresh of parial

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,7 +6,7 @@ class NotesController < ApplicationController
     note = Note.new(a_member_of: params[:page_id])
     saved_note = Valkyrie.config.metadata_adapter.persister.save(resource: note)
     respond_to do |format|
-      format.json { render json: { :note=> saved_note.noid } }
+      format.json { render json: { id: saved_note.noid } }
     end
   end
 end
