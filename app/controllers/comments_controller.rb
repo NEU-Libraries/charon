36752d3841736d0054a8388c57721c46d6f19464
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2021-09-10 11:43:06 -0400

    Stubbing out js create for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(a_member_of: params[:note_id])
+    saved_comment = Valkyrie.config.metadata_adapter.persister.save(resource: comment)
+    respond_to do |format|
+      format.json { render json: { id: saved_comment.noid } }
+    end
+  end
+end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,7 +2,6 @@
 
 class NotesController < ApplicationController
   def create
-    # id = page_id
     note = Note.new(a_member_of: params[:page_id])
     saved_note = Valkyrie.config.metadata_adapter.persister.save(resource: note)
     respond_to do |format|
