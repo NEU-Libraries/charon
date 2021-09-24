26bc9473effd41439a6d2f575c2305fc203417ac
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2021-09-24 11:38:08 -0400

    Remote comment submission working. Need to include note id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,12 @@
 
 class CommentsController < ApplicationController
   def create
-    comment = Comment.new(a_member_of: params[:note_id])
-    saved_comment = Valkyrie.config.metadata_adapter.persister.save(resource: comment)
-    respond_to do |format|
-      format.json { render json: { id: saved_comment.noid } }
-    end
+    # comment = Comment.new(a_member_of: params[:note_id])
+    # saved_comment = Valkyrie.config.metadata_adapter.persister.save(resource: comment)
+    # respond_to do |format|
+    #   format.json { render json: { id: saved_comment.noid } }
+    # end
+    logger.info params.inspect
   end
 
   def destroy
