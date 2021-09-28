8b87b0ed085f984ebabbc07bbdaba10b2bee9ce0
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2021-09-28 15:11:09 -0400

    Rubocop cleanup

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,11 +3,7 @@
 class NotesController < ApplicationController
   def create
     @page = Page.find(params[:page_id])
-    note = Note.new(alternate_ids: [Valkyrie::ID.new(params[:noid])],
-                    a_member_of: params[:page_id],
-                    region: params[:latlng],
-                    color: params[:color])
-    Valkyrie.config.metadata_adapter.persister.save(resource: note)
+    create_note
     respond_to do |format|
       format.js { render 'notes/_table', layout: false and return }
     end
@@ -17,4 +13,14 @@ class NotesController < ApplicationController
     @note = Note.find(params[:id])
     Valkyrie.config.metadata_adapter.persister.delete(resource: @note)
   end
+
+  private
+
+    def create_note
+      note = Note.new(alternate_ids: [Valkyrie::ID.new(params[:noid])],
+                      a_member_of: params[:page_id],
+                      region: params[:latlng],
+                      color: params[:color])
+      Valkyrie.config.metadata_adapter.persister.save(resource: note)
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,18 +4,23 @@ class PagesController < ApplicationController
   def show
     # convert notes to consumbale JSON
     page = Page.find(params[:id])
-    note_hsh = {}
-    note_hsh[:notes] = []
-    page.notes.each do |n|
-      note_vals = {
-        'noid' => n.noid,
-        'color' => n.color,
-        'region' => JSON.parse(n.region)
-      }
-      note_hsh[:notes] << note_vals
-    end
     respond_to do |format|
-      format.json { render json: note_hsh }
+      format.json { render json: build_json(page) }
     end
   end
+
+  private
+
+    def build_json(page)
+      note_hsh = { notes: [] }
+      page.notes.each do |n|
+        note_vals = {
+          'noid' => n.noid,
+          'color' => n.color,
+          'region' => JSON.parse(n.region)
+        }
+        note_hsh[:notes] << note_vals
+      end
+      note_hsh
+    end
 end
